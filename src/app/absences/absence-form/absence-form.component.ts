dabd307ec6fa7d1247b356bd5a26aa69a77bf2fe
Ihor Reva <igoryan.reva.01@icloud.com>
2025-03-08 22:54:54 +0100

    added material date picker

--- src/app/absences/absence-form/absence-form.component.ts
+++ src/app/absences/absence-form/absence-form.component.ts
@@ -1,32 +1,58 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
-import { FormsModule  } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Absence } from '../absence-list/absence-list.models';
 
 @Component({
   selector: 'app-absence-form',
   standalone: true,
-  imports: [NgIf, NgFor, FormsModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    NgIf,
+    NgFor,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatIconModule,
+  ],
   templateUrl: './absence-form.component.html',
-  styleUrl: './absence-form.component.css'
+  styleUrl: './absence-form.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AbsenceFormComponent {
   @Input() isVisible = false;
   @Output() closeModal = new EventEmitter<Absence>();
+  @Output() submitModal = new EventEmitter();
   types: string[] = ['Option 1', 'Option 2', 'Option 3'];
   name: string = 'Absence';
   selectedType: string = this.types[0];
   startDate: string = '';
   endDate: string = '';
 
-  close(){
-    this.closeModal.emit(
+  close() {
+    this.closeModal.emit();
+  }
+
+  submit() {
+    this.submitModal.emit(
       new Absence(
-        0, 
-        this.name, 
-        this.selectedType, 
-        new Date(this.startDate), 
-        new Date(this.endDate), 
+        0,
+        this.name,
+        this.selectedType,
+        new Date(this.startDate),
+        new Date(this.endDate),
         1
       )
     );
--- src/app/absences/absence-list/absence-list.component.ts
+++ src/app/absences/absence-list/absence-list.component.ts
@@ -8,27 +8,40 @@ import { NgFor, DatePipe } from '@angular/common';
   standalone: true,
   imports: [DatePipe, NgFor, AbsenceFormComponent],
   templateUrl: './absence-list.component.html',
-  styleUrl: './absence-list.component.css'
+  styleUrl: './absence-list.component.css',
 })
 export class AbsenceListComponent implements OnInit {
   public absences: Absence[] = [];
   isFormOpened: boolean = false;
 
   ngOnInit(): void {
-    this.isFormOpened = true;
-    this.absences.push(new Absence(1, 'Item1', 'Type1', new Date(), new Date(), 1));
-    this.absences.push(new Absence(2, 'Item2', 'Type2', new Date(), new Date(), 1));
-    this.absences.push(new Absence(3, 'Item3', 'Type3', new Date(), new Date(), 1));
-    this.absences.push(new Absence(4, 'Item4', 'Type1', new Date(), new Date(), 1));
-    this.absences.push(new Absence(5, 'Item5', 'Type4', new Date(), new Date(), 1));
+    this.absences.push(
+      new Absence(1, 'Item1', 'Type1', new Date(), new Date(), 1)
+    );
+    this.absences.push(
+      new Absence(2, 'Item2', 'Type2', new Date(), new Date(), 1)
+    );
+    this.absences.push(
+      new Absence(3, 'Item3', 'Type3', new Date(), new Date(), 1)
+    );
+    this.absences.push(
+      new Absence(4, 'Item4', 'Type1', new Date(), new Date(), 1)
+    );
+    this.absences.push(
+      new Absence(5, 'Item5', 'Type4', new Date(), new Date(), 1)
+    );
   }
 
   openForm() {
     this.isFormOpened = true;
   }
 
-  closeForm(absence: Absence) {
-    this.absences.push(absence);
+  closeForm() {
     this.isFormOpened = false;
+  }
+
+  submitForm(absence: Absence) {
+    this.absences.push(absence);
+    this.closeForm();
   }
 }
