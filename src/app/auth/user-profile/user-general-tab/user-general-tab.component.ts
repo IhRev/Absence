30f7a745e77f2fa1c65b3f0bf9d0e7c5a6b078dc
Ihor Reva <igoryan.reva.01@icloud.com>
2025-05-14 20:45:34 +0200

    added change password

--- src/app/auth/user-profile/user-general-tab/user-general-tab.component.ts
+++ src/app/auth/user-profile/user-general-tab/user-general-tab.component.ts
@@ -1,27 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { PasswordConfirmationComponent } from '../../../common/password-confirmation/password-confirmation.component';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
 import { UserDetails } from '../../models/auth.models';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-general-tab',
   standalone: true,
-  imports: [PasswordConfirmationComponent, FormsModule],
+  imports: [FormsModule],
   templateUrl: './user-general-tab.component.html',
   styleUrl: './user-general-tab.component.css',
 })
 export class UserGeneralTabComponent implements OnInit {
   private authService: AuthService;
-  private router: Router;
   public details: UserDetails = null!;
-  public confirmationOpened: boolean = false;
 
-  constructor(authService: AuthService, router: Router) {
+  constructor(authService: AuthService) {
     this.authService = authService;
-    this.router = router;
   }
+
   public ngOnInit(): void {
     this.authService.getUserDetails().subscribe({
       next: (details) => {
@@ -32,31 +28,10 @@ export class UserGeneralTabComponent implements OnInit {
     });
   }
 
-  public logout(): void {
-    this.authService.logout().subscribe({
-      next: (isSuccess) => {
-        if (isSuccess) {
-          this.router.navigate(['/home']);
-        }
-      },
-    });
-  }
-
   public edit(): void {
     this.authService.updateUserDetails(this.details).subscribe({
       next: (isSuccess) => {
         if (isSuccess) {
-        }
-      },
-    });
-  }
-
-  public confirmationSubmitted(password: string): void {
-    this.confirmationOpened = false;
-    this.authService.deleteProfile(password).subscribe({
-      next: (isSuccess) => {
-        if (isSuccess) {
-          this.router.navigate(['/home']);
         }
       },
     });
--- src/app/auth/user-profile/user-security-tab/user-security-tab.component.ts
+++ src/app/auth/user-profile/user-security-tab/user-security-tab.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ChangePasswordRequest } from '../../models/auth.models';
+import { Router } from '@angular/router';
+import { PasswordConfirmationComponent } from '../../../common/password-confirmation/password-confirmation.component';
 
 @Component({
   selector: 'app-user-security-tab',
   standalone: true,
-  imports: [],
+  imports: [PasswordConfirmationComponent, FormsModule],
   templateUrl: './user-security-tab.component.html',
-  styleUrl: './user-security-tab.component.css'
+  styleUrl: './user-security-tab.component.css',
 })
 export class UserSecurityTabComponent {
+  private authService: AuthService;
+  private router: Router;
+  public oldPassword: string = '';
+  public newPassword: string = '';
+  public confirmationOpened: boolean = false;
 
+  public constructor(authService: AuthService, router: Router) {
+    this.authService = authService;
+    this.router = router;
+  }
+
+  public submit(): void {
+    this.authService
+      .changePassword(
+        new ChangePasswordRequest(this.oldPassword, this.newPassword)
+      )
+      .subscribe({
+        next: (isSuccess) => {
+          if (isSuccess) {
+            this.router.navigate(['/login']);
+          }
+        },
+      });
+  }
+
+  public confirmationSubmitted(password: string): void {
+    this.confirmationOpened = false;
+    this.authService.deleteProfile(password).subscribe({
+      next: (isSuccess) => {
+        if (isSuccess) {
+          this.router.navigate(['/home']);
+        }
+      },
+    });
+  }
+
+  public logout(): void {
+    this.authService.logout().subscribe({
+      next: (isSuccess) => {
+        if (isSuccess) {
+          this.router.navigate(['/home']);
+        }
+      },
+    });
+  }
 }
