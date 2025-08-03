b5ce04b95e8540cb733bd3f16a9c18b89ed6031d
Ihor Reva <igoryan.reva.01@icloud.com>
2025-08-03 14:50:14 +0200

    security tab finished

--- src/app/common/password-confirmation/password-confirmation.component.ts
+++ src/app/common/password-confirmation/password-confirmation.component.ts
@@ -1,11 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
+import { getErrorMessage, getErrors } from '../services/error-utilities';
 
 @Component({
   selector: 'app-password-confirmation',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, ReactiveFormsModule, NgFor],
   templateUrl: './password-confirmation.component.html',
   styleUrl: './password-confirmation.component.css',
 })
@@ -14,6 +21,14 @@ export class PasswordConfirmationComponent {
   @Output() public submitted = new EventEmitter<string>();
   @Output() public closed = new EventEmitter();
   public password: string = '';
+  public form = new FormGroup({
+    password: new FormControl('', [Validators.minLength(8)]),
+  });
+  public get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched && this.form.controls.password.dirty
+    );
+  }
 
   public submit(): void {
     this.submitted.emit(this.password);
@@ -22,4 +37,10 @@ export class PasswordConfirmationComponent {
   public close(): void {
     this.closed.emit();
   }
+
+  public getErrors(columnName: string) {
+    return getErrors(this.form, columnName);
+  }
+
+  public getErrorMessage = getErrorMessage;
 }
