d8d9184c29b4d11fc39d0ca589021a8349cdb8a4
Ihor Reva <igoryan.reva.01@icloud.com>
2025-08-06 21:52:15 +0200

    home page for logged in user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,39 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/services/auth.service';
+import { NgIf } from '@angular/common';
+import { UserDetails } from '../auth/models/auth.models';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements AfterViewInit, OnInit {
+  public loggedIn: boolean = false;
+  public user: UserDetails | null = null;
+
+  constructor(private readonly authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.loggedIn$.subscribe((value) => {
+      this.loggedIn = value;
+      if (value) {
+        this.authService.getUserDetails().subscribe({
+          next: (result) => {
+            if (result.isSuccess) {
+              this.user = result.data!;
+            }
+          },
+        });
+      } else {
+        this.user = null;
+      }
+    });
+  }
+
   public ngAfterViewInit(): void {
     const animationObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
